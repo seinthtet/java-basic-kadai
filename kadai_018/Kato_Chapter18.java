6cdda7ad37855506a5f57fd25ea43028cc37c821
Seint Htet <seinthtetsanntint@gmail.com>
2025-04-28 18:41:51 +0900

    kadai_018

---
+++ kadai_018/Kato_Chapter18.java
@@ -0,0 +1,26 @@
+package kadai_018;
+
+public abstract class Kato_Chapter18 {
+
+	public String familyName;
+	public String givenName;
+	public String address;
+	
+	public Kato_Chapter18 () {
+		this.familyName = "加藤";
+        this.address = "東京都中野区〇×";
+	}
+	
+	public void commonIntroduce () {
+		System.out.println("名前は " + familyName + givenName + " です");
+        System.out.println("住所は " + address + " です");
+	}
+	
+	public abstract void eachIntroduce();
+	
+	public void execIntroduce () {
+		commonIntroduce();
+		eachIntroduce();
+		System.out.println();
+	}
+}
