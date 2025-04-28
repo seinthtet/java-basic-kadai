19636f08dc4dd04d145630ee4d975ee43b1d9691
Seint Htet <seinthtetsanntint@gmail.com>
2025-04-28 18:45:08 +0900

    kadai_021

---
+++ kadai_021/DictionaryExec_Chapter21.java
@@ -0,0 +1,17 @@
+package kadai_021;
+
+public class DictionaryExec_Chapter21 {
+
+	public static void main(String[] args) {
+
+		Dictionary_Chapter21 dictionary = new Dictionary_Chapter21();
+
+		String[] wordsToCheck = { "apple", "banana", "grape", "orange" };
+
+		for (String word : wordsToCheck) {
+			dictionary.searchWord(word);
+		}
+
+	}
+
+}
